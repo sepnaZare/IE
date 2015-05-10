49ff5ad3d26959df945ee135c7b8aec1a1162320
Fatemeh Fathi <fatemeh83fathi@gmail.com>
2015-05-11 01:52:51 +0430

    home page nesfe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
 	def index
+		@items = Item.search(params[:search]) if params[:search].present?
+    	@items = Item.all if not params[:search].present?
+    	@stores = Store.search(params[:search]) if params[:search].present?
+    	@stores = Store.all if not params[:search].present?
 	end
 end
